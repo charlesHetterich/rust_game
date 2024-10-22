0819927addb2e675db5a9439494ce8eea164daac
Charles Hetterich <hetterich.charles@gmail.com>
2024-10-21 21:03:54 -0500

    added commands for specifying player control and headless mode on launch, as well as ability to reset scene— next is a big (much needed) refactor!

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,26 @@ use bevy::input::mouse::MouseMotion;
 use bevy::prelude::*;
 use bevy::render::mesh::PrimitiveTopology;
 use bevy::render::render_asset::RenderAssetUsages;
-use bevy::window::{CursorGrabMode, PrimaryWindow};
+use bevy::window::{Cursor, CursorGrabMode, PrimaryWindow};
 use bevy_rapier3d::prelude::*;
 use nn::ModuleT;
 use rand::Rng;
+use std::env;
 use std::fs::File;
 use std::io::{BufWriter, Write};
 use tch::*;
 
+#[derive(Resource, Default, Debug)]
+struct ProgramInputs {
+    headless: bool,
+    ai_control: bool,
+}
+
 fn main() {
     let mut app = App::new();
-    //  plugins
     app.add_plugins(DefaultPlugins)
-        .add_plugins(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f64(
-            1.0 / 60.0,
-        )))
         .add_plugins(RapierDebugRenderPlugin::default())
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
-        // add resources
-        .insert_resource(AggBallPositions::default())
         // startup systems
         .add_systems(Startup, general_setup)
         .add_systems(Startup, setup_graphics)
@@ -32,15 +33,37 @@ fn main() {
         .add_systems(Startup, start_cursor_toggle_grab)
         .add_systems(Startup, load_model)
         // update systems
-        .add_systems(Update, handle_move_camera) // Add the camera movement system
-        .add_systems(Update, watch_cursor_toggle_grab)
-        .add_systems(Update, toggle_debug_render)
-        // .add_systems(Update, move_controllable_ball_with_keyboard)
-        .add_systems(Update, move_controllable_ball_with_ai)
-        .add_systems(Update, apply_ball_drag)
-        .add_systems(Update, track_ball_positions)
-        .add_systems(Update, check_simulation_end)
-        .run();
+        .add_systems(Update, apply_ball_drag);
+
+    // capture program inputs
+    let args: Vec<String> = env::args().collect();
+    let program_inputs = ProgramInputs {
+        headless: args.contains(&"--headless".to_string()),
+        ai_control: args.contains(&"--ai-control".to_string()),
+    };
+
+    // headless setup
+    if (&program_inputs).headless {
+        app.insert_resource(AggBallPositions::default())
+            .add_systems(Update, check_simulation_end)
+            .add_systems(Update, move_controllable_ball_with_ai)
+            .add_systems(Update, track_ball_positions)
+            .add_plugins(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f64(
+                1.0 / 60.0,
+            )));
+    // regular setup
+    } else {
+        app.add_systems(Update, apply_system_inputs);
+        if (&program_inputs).ai_control {
+            app.add_systems(Update, move_controllable_ball_with_ai);
+        } else {
+            app.add_systems(Update, move_controllable_ball_with_keyboard);
+        }
+    }
+
+    // rest of general setup
+    app.insert_resource(program_inputs);
+    app.run();
 }
 
 #[derive(Resource)]
@@ -77,16 +100,18 @@ impl AggBallPositions {
 }
 
 fn track_ball_positions(
-    mut ball_positions: ResMut<AggBallPositions>,
+    mut ball_positions: Option<ResMut<AggBallPositions>>,
     query: Query<(Entity, &Transform), With<Ball>>,
 ) {
-    for (entity, transform) in query.iter() {
-        let position = (transform.translation.x, transform.translation.z);
-        ball_positions
-            .positions
-            .entry(entity)
-            .or_insert_with(Vec::new)
-            .push(position);
+    if let Some(ball_positions) = &mut ball_positions {
+        for (entity, transform) in query.iter() {
+            let position = (transform.translation.x, transform.translation.z);
+            ball_positions
+                .positions
+                .entry(entity)
+                .or_insert_with(Vec::new)
+                .push(position);
+        }
     }
 }
 
@@ -94,7 +119,7 @@ fn track_ball_positions(
 #[derive(Resource)]
 pub struct ModelResource {
     model: TrainableCModule,
-    vs: nn::VarStore,
+    _vs: nn::VarStore,
 }
 
 impl ModelResource {
@@ -103,32 +128,12 @@ impl ModelResource {
         let mut model =
             TrainableCModule::load(model_path, vs.root()).expect("Failed to load model");
         model.set_eval();
-        ModelResource { model, vs }
+        ModelResource { model, _vs: vs }
     }
 }
 fn load_model(mut commands: Commands) {
     let model_resource = ModelResource::new("src/modeling/ball_policy.pt");
     commands.insert_resource(model_resource);
-}
-
-// fn simulation_step(
-//     mut commands: Commands,
-//     time: ResMut<CustomTime>,
-//     mut rapier_context: ResMut<RapierPhysicsContext>,
-// ) {
-//     let time_step = 1.0 / 60.0; // 60 Hz, or adjust for higher resolution
-//     time.advance(time_step);
-//     rapier_context.step(time_step);
-// }
-
-fn toggle_debug_render(
-    // mut commands: Commands,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut debug_render_state: ResMut<DebugRenderContext>,
-) {
-    if keyboard_input.just_pressed(KeyCode::F1) {
-        debug_render_state.enabled = !debug_render_state.enabled;
-    }
 }
 
 fn check_simulation_end(
@@ -155,7 +160,6 @@ fn check_simulation_end(
 
 fn general_setup(mut commands: Commands) {
     // TODO : add a bunch of the other more basic setup stuff here
-
     // Add 10s game timer
     commands.insert_resource(SimulationTimer {
         timer: Timer::from_seconds(1.0, TimerMode::Once),
@@ -181,20 +185,6 @@ fn setup_physics(
 ) {
     let mut rng = rand::thread_rng();
 
-    // Add a light source
-    // commands.spawn(DirectionalLightBundle {
-    //     directional_light: DirectionalLight {
-    //         color: Color::srgb(1.0, 0.9, 0.9),
-    //         shadows_enabled: true,
-    //         illuminance: 5_000.0, // You can tweak this value for brightness
-    //         ..default()
-    //     },
-    //     transform: Transform {
-    //         rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_4), // Set rotation to simulate sunlight angle
-    //         ..default()
-    //     },
-    //     ..default()
-    // });
     // Add a light source
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -573,16 +563,6 @@ fn get_ai_movement(model: &TrainableCModule, input: Tensor) -> (bool, bool, bool
     let output = model.forward_t(&input, false);
     let output = output.sigmoid(); // Apply sigmoid function
 
-    // let up = output.double_value(&[0]) > 0.5;
-    // let down = output.double_value(&[1]) > 0.5;
-    // let left = output.double_value(&[2]) > 0.5;
-    // let right = output.double_value(&[3]) > 0.5;
-
-    // (up, down, left, right)
-
-    // let output = model.forward(&input);
-    // let output = output.sigmoid(); // Apply sigmoid function to convert logits to probabilities
-
     // Extract the 4 float values (logits) from the tensor
     let output_values = Vec::<f32>::try_from(
         output
@@ -591,8 +571,6 @@ fn get_ai_movement(model: &TrainableCModule, input: Tensor) -> (bool, bool, bool
             .to_kind(Kind::Float),
     )
     .unwrap();
-    // .vec
-    // .into_vec();
 
     // The AI model's output will be an array of 4 values
     let up = output_values[0] > 0.5;
@@ -607,7 +585,7 @@ fn get_ai_movement(model: &TrainableCModule, input: Tensor) -> (bool, bool, bool
 fn move_controllable_ball_with_ai(
     model_resource: Res<ModelResource>,
     mut param_set: ParamSet<(
-        Query<(&mut Velocity, &mut Transform), With<ControllableBall>>,
+        Query<&mut Velocity, With<ControllableBall>>,
         Query<(&Velocity, &Transform), With<Ball>>, // To get the state of all balls, excluding ControllableBall
     )>,
     time: Res<Time>, // To make movement frame rate independent
@@ -628,155 +606,155 @@ fn move_controllable_ball_with_ai(
     let input_tensor = Tensor::from_slice(&inputs).view([1, (50 + 1) * 4]); // Adjust for batch size of 1
     let (up, down, left, right) = get_ai_movement(&model_resource.model, input_tensor);
 
-    // print movement outputs
-    println!(
-        "up: {}, down: {}, left: {}, right: {}",
-        up, down, left, right
-    );
-
     // Step 3: Apply movement by borrowing the first query mutably
-    if let Ok((mut velocity, transform)) = param_set.p0().get_single_mut() {
-        println!("{:?}", transform.translation.x);
+    if let Ok(mut velocity) = param_set.p0().get_single_mut() {
+        // println!("{:?}", transform.translation.x);
         apply_movement(up, down, left, right, &mut velocity, &time);
     }
 }
-
-// fn move_controllable_ball(
-//     keyboard_input: Res<ButtonInput<KeyCode>>,
-//     mut query: Query<(&mut Velocity, &mut Transform), With<ControllableBall>>,
-//     time: Res<Time>, // To make movement frame rate independent
-// ) {
-//     if let Ok((mut velocity, mut transform)) = query.get_single_mut() {
-//         let mut direction = Vec3::ZERO;
-
-//         if keyboard_input.pressed(KeyCode::ArrowUp) {
-//             direction.z -= 1.0;
-//         }
-//         if keyboard_input.pressed(KeyCode::ArrowDown) {
-//             direction.z += 1.0;
-//         }
-//         if keyboard_input.pressed(KeyCode::ArrowLeft) {
-//             direction.x -= 1.0;
-//         }
-//         if keyboard_input.pressed(KeyCode::ArrowRight) {
-//             direction.x += 1.0;
-//         }
-
-//         // Normalize to prevent faster diagonal movement and set speed
-//         if direction.length_squared() > 0.0 {
-//             direction = direction.normalize() * 50000.0 * time.delta_seconds(); // Adjust speed as needed
-//         }
-
-//         // Apply translation
-//         velocity.linvel += direction * time.delta_seconds();
-
-//         // clamp velocity
-//         // let max_speed = 10.0; // Adjust as needed
-//         // if velocity.linvel.length_squared() > max_speed * max_speed {
-//         //     velocity.linvel = velocity.linvel.normalize() * max_speed;
-//         // }
-
-//         // Constrain within walls (example)
-//         let x_limit = 24.0; // Adjust based on your wall setup
-//         let z_limit = 24.0; // Adjust based on your wall setup
-
-//         transform.translation.x = transform.translation.x.clamp(-x_limit, x_limit);
-//         transform.translation.z = transform.translation.z.clamp(-z_limit, z_limit);
-//     }
-// }
 
 // Camera movement
 #[derive(Component)]
 struct CameraController;
 
-fn handle_move_camera(
+fn apply_system_inputs(
+    // mut commands: Commands,
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut mouse_motion_events: EventReader<MouseMotion>,
-    mut query: Query<&mut Transform, With<CameraController>>,
+    mouse_motion_events: EventReader<MouseMotion>,
     mut q_windows: Query<&mut Window, With<PrimaryWindow>>,
+    mut param_set: ParamSet<(
+        Query<&mut Transform, With<CameraController>>,
+        ParamSet<(
+            Query<(&mut Velocity, &mut Transform), With<Ball>>,
+            Query<(&mut Velocity, &mut Transform), With<ControllableBall>>,
+        )>, // To get the state of all balls, excluding ControllableBall
+    )>,
+    debug_render_state: ResMut<DebugRenderContext>,
 ) {
-    let primary_window = q_windows.single_mut();
-    if primary_window.cursor.grab_mode == CursorGrabMode::None {
-        return;
+    let mut primary_window = q_windows.single_mut();
+
+    // toggle rapier debug render
+    if (&keyboard_input).just_pressed(KeyCode::F2) {
+        toggle_debug_render(debug_render_state)
     }
-    move_camera(keyboard_input, mouse_motion_events, query);
+
+    // toggle camera control & mouse lock
+    if (&keyboard_input).just_pressed(KeyCode::Escape) {
+        cursor_toggle_grab(&mut primary_window.cursor);
+    }
+
+    // move camera
+    if primary_window.cursor.grab_mode == CursorGrabMode::Locked {
+        move_camera(
+            &keyboard_input,
+            mouse_motion_events,
+            (&mut param_set.p0()).single_mut(),
+        );
+    }
+
+    // reset scene
+    if (&keyboard_input).just_pressed(KeyCode::KeyR) {
+        reset_scene(param_set.p1());
+    }
+}
+
+fn toggle_debug_render(
+    // mut commands: Commands,
+    mut debug_render_state: ResMut<DebugRenderContext>,
+) {
+    debug_render_state.enabled = !debug_render_state.enabled;
+}
+
+fn reset_scene(
+    mut param_set: ParamSet<(
+        Query<(&mut Velocity, &mut Transform), With<Ball>>,
+        Query<(&mut Velocity, &mut Transform), With<ControllableBall>>,
+    )>,
+) {
+    let mut rng = rand::thread_rng();
+    // let ball_radius = 0.5;
+    let width = 25.0; // Assuming the width of the area
+    let length = 25.0; // Assuming the length of the area
+
+    // reset other balls
+    for (mut velocity, mut transform) in param_set.p0().iter_mut() {
+        let x_max = width / 2.0 - 1.0;
+        let z_max = length / 2.0 - 1.0;
+        let x_pos = rng.gen_range(-x_max..x_max);
+        let z_pos = rng.gen_range(-z_max..z_max);
+
+        transform.translation = Vec3::new(x_pos, 0.0, z_pos);
+        velocity.linvel = Vec3::new(0.0, 0.0, 0.0);
+    }
+
+    // Reset controllable ball
+    for (mut velocity, mut transform) in param_set.p1().iter_mut() {
+        transform.translation = Vec3::new(0.0, 0.0, 0.0);
+        velocity.linvel = Vec3::new(0.0, 0.0, 0.0);
+    }
 }
 
 fn move_camera(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
+    keyboard_input: &Res<ButtonInput<KeyCode>>,
     mut mouse_motion_events: EventReader<MouseMotion>,
-    mut query: Query<&mut Transform, With<CameraController>>,
+    mut camera_transform: Mut<'_, Transform>,
 ) {
-    for mut transform in query.iter_mut() {
-        // Handle keyboard input for movement
-        let forward = Vec3::new(transform.forward().x, 0.0, transform.forward().z).normalize();
-        let right = Vec3::new(transform.right().x, 0.0, transform.right().z).normalize();
-        let up = Vec3::Y;
+    let transform = &mut camera_transform;
 
-        if keyboard_input.pressed(KeyCode::KeyW) {
-            transform.translation += forward * 0.1; // Move the camera forward
-        }
-        if keyboard_input.pressed(KeyCode::KeyS) {
-            transform.translation -= forward * 0.1; // Move the camera backward
-        }
-        if keyboard_input.pressed(KeyCode::KeyA) {
-            transform.translation -= right * 0.1; // Move the camera left
-        }
-        if keyboard_input.pressed(KeyCode::KeyD) {
-            transform.translation += right * 0.1; // Move the camera right
-        }
-        if keyboard_input.pressed(KeyCode::ShiftLeft) {
-            transform.translation -= up * 0.1; // Move the camera down
-        }
-        if keyboard_input.pressed(KeyCode::Space) {
-            transform.translation += up * 0.1; // Move the camera up
-        }
+    let forward = Vec3::new(transform.forward().x, 0.0, transform.forward().z).normalize();
+    let right = Vec3::new(transform.right().x, 0.0, transform.right().z).normalize();
+    let up = Vec3::Y;
 
-        // Handle mouse motion for rotation
-        for event in mouse_motion_events.read() {
-            let delta_x = event.delta.x * 0.1; // Adjust sensitivity as needed
-            let delta_y = event.delta.y * 0.1; // Adjust sensitivity as needed
+    if keyboard_input.pressed(KeyCode::KeyW) {
+        transform.translation += forward * 0.1; // Move the camera forward
+    }
+    if keyboard_input.pressed(KeyCode::KeyS) {
+        transform.translation -= forward * 0.1; // Move the camera backward
+    }
+    if keyboard_input.pressed(KeyCode::KeyA) {
+        transform.translation -= right * 0.1; // Move the camera left
+    }
+    if keyboard_input.pressed(KeyCode::KeyD) {
+        transform.translation += right * 0.1; // Move the camera right
+    }
+    if keyboard_input.pressed(KeyCode::ShiftLeft) {
+        transform.translation -= up * 0.1; // Move the camera down
+    }
+    if keyboard_input.pressed(KeyCode::Space) {
+        transform.translation += up * 0.1; // Move the camera up
+    }
 
-            // Rotate around the Y axis (yaw)
-            transform.rotation = Quat::from_rotation_y(-delta_x.to_radians()) * transform.rotation;
+    // Handle mouse motion for rotation
+    for event in mouse_motion_events.read() {
+        let delta_x = event.delta.x * 0.1; // Adjust sensitivity as needed
+        let delta_y = event.delta.y * 0.1; // Adjust sensitivity as needed
 
-            // Rotate around the X axis (pitch)
-            transform.rotation = transform.rotation * Quat::from_rotation_x(-delta_y.to_radians());
-        }
+        // Rotate around the Y axis (yaw)
+        transform.rotation = Quat::from_rotation_y(-delta_x.to_radians()) * transform.rotation;
+
+        // Rotate around the X axis (pitch)
+        transform.rotation = transform.rotation * Quat::from_rotation_x(-delta_y.to_radians());
     }
 }
 
 fn start_cursor_toggle_grab(mut q_windows: Query<&mut Window, With<PrimaryWindow>>) {
-    let primary_window = q_windows.single_mut();
-    cursor_toggle_grab(primary_window);
+    cursor_toggle_grab(&mut q_windows.single_mut().cursor);
 }
 
-fn watch_cursor_toggle_grab(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut q_windows: Query<&mut Window, With<PrimaryWindow>>,
-) {
-    if keyboard_input.just_pressed(KeyCode::Escape) {
-        let primary_window = q_windows.single_mut();
-        cursor_toggle_grab(primary_window);
-    }
-}
-
-fn cursor_toggle_grab(mut window: Mut<'_, Window>) {
-    match window.cursor.grab_mode {
+fn cursor_toggle_grab(cursor: &mut Cursor) {
+    match cursor.grab_mode {
         CursorGrabMode::None => {
-            window.cursor.grab_mode = CursorGrabMode::Locked;
-            window.cursor.visible = false;
+            cursor.grab_mode = CursorGrabMode::Locked;
+            cursor.visible = false;
         }
         _ => {
-            window.cursor.grab_mode = CursorGrabMode::None;
-            window.cursor.visible = true;
+            cursor.grab_mode = CursorGrabMode::None;
+            cursor.visible = true;
         }
     }
 }
 
 fn setup_ui(mut commands: Commands) {
-    // commands.spawn(UiCameraBundle::default());
-
     commands
         .spawn(NodeBundle {
             style: Style {
@@ -800,8 +778,6 @@ fn setup_ui(mut commands: Commands) {
                     style: Style {
                         width: Val::Px(10.0),
                         height: Val::Px(10.0),
-                        // size: Size::new(Val::Px(10.0), Val::Px(10.0)),
-                        // border: Rect::all(Val::Px(1.0)),
                         ..Default::default()
                     },
                     border_radius: BorderRadius::all(Val::Px(5.0)),
